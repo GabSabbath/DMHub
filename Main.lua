f8d4fb9f1b99e6e7678978a2b9224bc014913b78
Gabriel Brunet <gabriel.brunet.1@ens.etsmtl.ca>
2022-12-04 21:40:01 -0500

    Added Title to panel and put sample search bar

--- Main.lua
+++ Main.lua
@@ -3,22 +3,55 @@ local mod = dmhub.GetModLoading()
 --#region class JournalPannel
 local JournalPannel = {}
 
+function JournalPannel:SearchInput()
+	return gui.Input{
+		id = 'JournalSearch',
+		placeholderText = 'Search ...',
+		editlag = 0.25,
+		style = {
+			fontSize = '50%',
+			width = '80%',
+			height = '80%',
+			halign = 'left',
+			valign = 'center',
+		},
+
+		-- events = {
+		-- 	edit = updateSearch,
+		-- 	change = updateSearch,
+		-- }
+	}
+end
+
 function JournalPannel:new ()
     return gui.Panel{
 		height = "auto",
 		width = "auto",
-        gui.Label{
-			height = "auto",
-			width = "auto",
-            text = "This is the journal"
-        }
+		flow = 'vertical',
+		children = {
+			gui.Label{
+				height = "auto",
+				width = "auto",
+				text = "Journal",
+				halign = "left",
+				color = "#d4d1ba",
+				fontFace = "dubai",
+				uppercase = true,
+				fontSize = 14,
+				bold = true,
+			},
+			JournalPannel:SearchInput(),
+			
+		}
     }
 end
+
 --#endregion
 
 --#region putting the panel in DMHub
 
 DockablePanel.Register{
+	dmonly=false,
 	name = "Journal",
 	-- icon = mod.images.bestiaryIcon,
 	minHeight = 140,
@@ -28,3 +61,5 @@ DockablePanel.Register{
 		return JournalPannel:new ()
 	end
 }
+
+--#endregion
